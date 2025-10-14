381cdd31c552e1c4cf1a769335287eb390d6d72a
T-Yanagitani <minf_rec@outlook.com>
2025-10-14 17:26:05 +0900

    20251014

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,15 +13,20 @@ class ReportController extends Controller
     public function index()
     {
         //
+		// 重要な投稿
 		// $importance = Report::withCount('comments')->where('importance', '>=', '2')->latest()->take(5)->get();
-		$importance = Report::where('importance', '>=', '2')->latest()->paginate(5);
+		// $importance = Report::where('importance', '>=', '2')->latest()->paginate(5);
+		$importance = Report::where( 'importance', '>=', '2' )->withCount( 'comments' )->latest()->paginate(5);
+
+		// 通常投稿最新5件
 		// $articles = Report::withTrashed()->withCount('comments')->latest()->take(5)->get();
 		// $articles = Report::withCount('comments')->latest()->take(5)->get();
-		$articles = Report::latest()->paginate(5);
+		// $articles = Report::latest()->paginate(5);
+		$articles = Report::withCount( 'comments' )->latest()->paginate(5);
 
-		$today = date('西暦Y年m月d日（D）');
+		$today = date( '西暦Y年m月d日（D）' );
 
-		return view('index', ['importance' => $importance, 'articles' => $articles, 'today' => $today]);
+		return view( 'index', ['importance' => $importance, 'articles' => $articles, 'today' => $today] );
     }
 
     /**
@@ -34,11 +39,14 @@ class ReportController extends Controller
     /**
 	 * Store a newly created resource in storage.
      */
-	public function store(Request $request)
+	// 記事投稿（データベース格納）
+	public function store( Request $request )
     {
 		//
 		$image_path = NULL;
 
+		// 投稿画像レギュレーション
+		// 縦横1920px（1080p想定）、2MB上限
 		// $request->validate(['image' => 'image|dimensions:max_width=1920, max_height=1080|max:4096']);
 		$request->validate(['image' => [
 			'image',
@@ -76,10 +84,11 @@ class ReportController extends Controller
     /**
 	 * Show the form for editing the specified resource.
      */
-	public function edit(Report $report, $id)
+	// 記事編集
+	public function edit( $id )
     {
 		//
-		$articles = Report::find($id);
+		$articles = Report::find( $id );
 
 		return view( 'edit', ['articles' => $articles, 'id' => $id] );
     }
@@ -87,7 +96,8 @@ class ReportController extends Controller
     /**
 	 * Update the specified resource in storage.
      */
-	public function update(Request $request, Report $report)
+	// 記事編集格納
+	public function update( Request $request, Report $report )
     {
 		//
 		$report = Report::find( $request->id );
@@ -100,8 +110,8 @@ class ReportController extends Controller
     /**
 	 * Remove the specified resource from storage.
      */
-	// 記事削除（物理削除）
-	public function destroy(Report $report, $id)
+	// 記事削除（物理削除：データベース上から完全削除）
+	public function destroy( Report $report, $id )
     {
 		//
 		$article = Report::find( $id );
@@ -112,26 +122,22 @@ class ReportController extends Controller
 
 	// 投稿一覧
 	public function report_list( Request $request ) {
-		$order = $request->query('order');
+		$order = $request->query( 'order' );
 		if( !isset( $order ) ) $order = 'desc';
 
-		$articles = Report::orderBy('id', $order)->paginate(10);
+		$articles = Report::orderBy( 'id', $order )->paginate(10);
 
-		// 投稿総数
-		$count = Report::count();
-
-		return view('report_list', ['articles' => $articles, 'order' => $order]);
+		return view('report_list', ['articles' => $articles, 'order' => $order] );
 	}
 
 	// 記事検索
 	public function search( Request $request ) {
 		$keywords = array_filter( explode( " ", trim( $request->query( 'keyword' ) ) ) );
 
-		
 		if( !empty( $keywords ) ) {
 			$mode = match( $request->query('mode') ) {
-				'1' => 'AND',
-				'2' => 'OR',
+				'1',1 => 'AND',
+				'2',1 => 'OR',
 				default => 'AND'
 			};
 
@@ -141,14 +147,14 @@ class ReportController extends Controller
 				// AND検索
 				foreach( $keywords as $keyword ) {
 					$query->where( function ( $q ) use ( $keyword ) {
-						$q->where('article', 'LIKE', "%{$keyword}%");
+						$q->where( 'article', 'LIKE', "%{$keyword}%" );
 					});
 				}
 			} elseif ( $mode === 'OR' ) {
 				// OR検索
 				foreach( $keywords as $keyword ) {
 					$query->orWhere( function ( $q ) use ( $keyword ) {
-						$q->orWhere('article', 'LIKE', "%{$keyword}%");
+						$q->orWhere( 'article', 'LIKE', "%{$keyword}%" );
 					});
 				}
 			}
@@ -156,7 +162,7 @@ class ReportController extends Controller
 			// $result = $query->take(10)->get();
 			$result = $query->paginate(10);
 
-			return view('result_list', ['articles' => $result, 'keywords' => $keywords, 'mode' => $mode ]);
+			return view( 'result_list', ['articles' => $result, 'keywords' => $keywords, 'mode' => $mode ] );
 		}
 
 		return redirect()->route( 'report.index' );
@@ -164,24 +170,24 @@ class ReportController extends Controller
 
 	// 投稿記事単体表示
 	public function detail( $id ) {
-		$articles = Report::with('comments')->withCount('comments')->find($id);
+		$articles = Report::with( 'comments' )->withCount( 'comments' )->find( $id );
 
 		$create_date = $articles->created_at->format( "Y/m/d/ H:i" );
-		return view('detail', ['articles' => $articles, 'create_date' => $create_date, 'id' => $id]);
+		return view( 'detail', ['articles' => $articles, 'create_date' => $create_date, 'id' => $id] );
 	}
 
 	// 新規記事投稿フォーム
 	public function write() {
-		return view('write');
 		// return view('write', ['mode' => $mode]);
+		return view( 'write' );
 	}
 
-	// 記事削除（ソフトデリート）
+	// 記事削除（ソフトデリート：データベースからは削除しない）
 	public function delete( $id ) {
 		$article = Report::find( $id );
 		$article->delete();
 
-		return redirect()->route('report.report_list');
+		return redirect()->route( 'report.report_list' );
 	}
 
 	// 記事復元（ソフトデリートロールバック）
@@ -189,14 +195,14 @@ class ReportController extends Controller
 		$article = Report::find( $id );
 		$article->restore();
 
-		return redirect()->route('report.detail', ['id' => $id] );
+		return redirect()->route( 'report.detail', ['id' => $id] );
 	}
 	
 	// テスト
-	public function foo(Request $request) {
+	public function foo( Request $request ) {
 		$comment = 'Debug message!!';
 		// $comment = $request->comment;
-		return view('foo', ['comment' => $comment]);
+		return view( 'foo', ['comment' => $comment] );
 	}
 
 }
